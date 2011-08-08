37fb721b67de08df2a0562d4c492c3c97ace1f77
Ulric Wilfred <shaman.sir@gmail.com>
2011-08-08 13:42:49 +0300

    coool extended build.xml

--- pack-eclipse-files.sh
+++ pack-eclipse-files.sh
@@ -7,8 +7,8 @@ fi
 echo "API version: $API_VER"
 
 START_DIR=`pwd`
-cd ./apk-API$API_VER
-zip ../eclipse-files-API$API_VER.zip \
+cd ./sources/apk-API$API_VER
+zip ../../eclipse-files-API$API_VER.zip \
     ./.classpath \
     ./.project
 cd $START_DIR
---
+++ release-all.sh
@@ -0,0 +1,19 @@
+#APP_VER=`cat version`
+
+#if [ -z "$APP_VER" ]; then
+#   echo "no version file found!"
+#   exit
+#fi
+
+#echo "Application version: $APP_VER"
+
+START_DIR=`pwd`
+for i in 7
+do
+   API_VER=$i
+   echo "Building for API: $API_VER"
+   cd $START_DIR/sources/apk-API$API_VER
+   ant release-and-get
+done
+
+cd $START_DIR
