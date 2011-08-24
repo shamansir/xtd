9019a24cc43c8413086e45fe75fab7d5f1178b72
Ulric Wilfred <shaman.sir@gmail.com>
2011-08-24 12:21:18 +0300

    exception info to show

--- sources/js/test-mdown-parser-with-node.js
+++ sources/js/test-mdown-parser-with-node.js
@@ -3,10 +3,14 @@ var PEG = require('pegjs');
 
 require('./parser-defs');
 
-var parser = PEG.buildParser(
+try {
+    var parser = PEG.buildParser(
                   fs.readFileSync('/home/shamansir/Workspace/xtd/sources/peg/markdown.pegjs', 'utf-8'));
-var testContent = fs.readFileSync('/home/shamansir/Worktable/peg-markdown-highlight/testfiles/bom.md', 'utf-8');
+    var testContent = fs.readFileSync('/home/shamansir/Worktable/peg-markdown-highlight/testfiles/bom.md', 'utf-8');
 
-var result = parser.parse(testContent);
-console.log(result);
+   var result = parser.parse(testContent);
+   console.log(result);
+} catch(e) {
+   console.log(e);
+}
 
