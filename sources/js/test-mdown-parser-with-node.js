f85e03ec8d8cd481a073ffd8f2185c473e98daeb
Ulric Wilfred <shaman.sir@gmail.com>
2011-08-30 01:16:08 +0300

    nice paths in js test file

--- sources/js/test-mdown-parser-with-node.js
+++ sources/js/test-mdown-parser-with-node.js
@@ -5,11 +5,21 @@ require('./parser-defs');
 
 try {
     var parser = PEG.buildParser(
-                  fs.readFileSync('/home/shamansir/Workspace/xtd/sources/peg/markdown.pegjs', 'utf-8'));
-    var testContent = fs.readFileSync('/home/shamansir/Worktable/peg-markdown-highlight/testfiles/bom.md', 'utf-8');
+                  fs.readFileSync('../peg/markdown.pegjs', 'utf-8'));
+    var testContent = fs.readFileSync('../peg/mdown-test/testfiles/bom.md', 'utf-8');
 
+   /* var parser = PEG.buildParser(
+      'start = ruleOne' + '\n\n' +
+      'ruleOne = a:ruleTwo { console.log("a => " + a); return a; }' + '\n\n' +
+      'ruleTwo = z:"a" (( b:("b" { return "x" }) "c" { return b + "y" } )' +
+                       '( "d" { return "w" } )*' +
+                       '( ("e") { return "m" } )*' +
+                      ')+' +
+                       '{ return "z ~~ " + z; }' );
+   var result = parser.parse("abcdddeee"); */
    var result = parser.parse(testContent);
    console.log(result);
+   console.log('=====');
 } catch(e) {
    console.log(e);
 }
