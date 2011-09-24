7e2f5321012bcedc2e7cdc21bf12d051c6587b86
Ulric Wilfred <shaman.sir@gmail.com>
2011-09-24 14:33:44 +0300

    also packing test project eclipse files

--- pack-eclipse-files.sh
+++ pack-eclipse-files.sh
@@ -6,9 +6,18 @@ fi
 
 echo "API version: $API_VER"
 
+rm ./eclipse-files-API$API_VER.zip
+rm ./eclipse-files-API$API_VER-test.zip
+
 START_DIR=`pwd`
 cd ./sources/apk-API$API_VER
 zip ../../eclipse-files-API$API_VER.zip \
+    ./.classpath \
+    ./.project
+cd $START_DIR
+
+cd ./sources/apk-API$API_VER-test
+zip ../../eclipse-files-API$API_VER-test.zip \
     ./.classpath \
     ./.project
 cd $START_DIR
